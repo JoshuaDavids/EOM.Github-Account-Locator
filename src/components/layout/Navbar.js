0be7e7b4700ed2c9099b0c653825d5b9d0ad9677
JoshuaDavids <joshuadavids.jcd@gmail.com>
2020-07-26 19:09:35 +0200

    updated styling

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
   return (
-    <nav className='navbar bg-primary'>
+    <nav className='navbar bg-color'>
       <h1>
         <i className={icon} /> {title}
       </h1>
--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,8 +1,14 @@
-import React, { Fragment } from 'react'
-import spinner from './spinner.gif';
+import React, { Fragment } from "react";
+import spinner from "./spinner.gif";
 
-const Spinner = () => <Fragment>
-      <img src={spinner} alt="Loading..." style={{ width: '200px', margin: 'auto', display: 'block' }} />
-    </Fragment>
+const Spinner = () => (
+  <Fragment>
+    <img
+      src={spinner}
+      alt="Loading..."
+      style={{ width: "200px", margin: "auto", display: "block" }}
+    />
+  </Fragment>
+);
 
-export default Spinner
+export default Spinner;
